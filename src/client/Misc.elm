3465be67468ac24d6648ce84d8b66ed1bb2d8e4d
rocka <i@rocka.me>
2018-02-02 14:29:37 +0800

    frontend: Misc: add helper function `onKeyDown` and `toIntDefault`

--- src/client/Misc.elm
+++ src/client/Misc.elm
@@ -1,9 +1,27 @@
-module Misc exposing ((=>))
+module Misc exposing (..)
+
+import String
+import Html
+import Html.Events
+import Json.Decode
+
 
 {-| ref: <https://github.com/rtfeldman/elm-spa-example/blob/4213d8cb6fe4d72f82b7cdd5bc43f64fab99ad12/src/Util.elm>
 -}
-
-
 (=>) : a -> b -> ( a, b )
 (=>) =
     (,)
+
+
+onKeyDown : (Int -> msg) -> Html.Attribute msg
+onKeyDown tagger =
+    Html.Events.on "keydown" (Json.Decode.map tagger Html.Events.keyCode)
+
+toIntDefault : Int -> String -> Int
+toIntDefault default str =
+    case String.toInt str of
+        Ok int ->
+            int
+
+        Err _ ->
+            default
