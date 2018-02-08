31cdcbea28659ac6cf4a9d0f7d2d4204d2c7c448
Rocka <i@rocka.me>
2018-02-08 15:13:51 +0800

    frontend: Misc: remove `toIntDefault`, use `Result.witiDefault` instead

--- src/client/Misc.elm
+++ src/client/Misc.elm
@@ -22,18 +22,6 @@ onKeyDown tagger =
     Html.Events.on "keydown" (Json.Decode.map tagger Html.Events.keyCode)
 
 
-{-| Convert String to Int; If there are any errors, just return the default value
--}
-toIntDefault : Int -> String -> Int
-toIntDefault default str =
-    case String.toInt str of
-        Ok int ->
-            int
-
-        Err _ ->
-            default
-
-
 {-| Build url with List of querystring parameters
 -}
 urlWithQuery : String -> List ( String, String ) -> String
